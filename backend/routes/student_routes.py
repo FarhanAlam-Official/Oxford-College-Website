a92847a5c32d8a2166441122adf61354fd7e1c1c
ryuzinoh <whysafalisop@gmail.com>
2024-12-08 21:48:40 +0545

    added login as a backeend

--- backend/routes/student_routes.py
+++ backend/routes/student_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from services.studentregister import reg_student
 from db import mongo
+from services.studentlogin import login_user
 
 #creating BP
 student_routes = Blueprint('student_routes', __name__)
@@ -14,3 +15,18 @@ def register():
     if response['success']:
         return jsonify({"message": "Student registered successfully", "id": response['id']}), 201
     return jsonify({"message": response['message']}), 400
+
+
+# Login route
+@student_routes.route('/login', methods=['POST'])
+def login():
+    login_data = request.json
+    response = login_user(login_data)  
+
+    if response['success']:
+        return jsonify({
+            "message": response['message'],
+            "token": response['token']
+        }), 200
+    else:
+        return jsonify({"message": response['message']}), 401
